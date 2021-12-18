54d7ae80e0c239edec5f0650d8215fbf1cda3593
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 18:42:21 -0500

    Create AbstractUnexpectedValueException.php

---
+++ Potter/Throwable/Exception/Runtime/UnexpectedValue/AbstractUnexpectedValueException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\Throwable\Exception\Runtime\UnexpectedValue;
+
+use \UnexpectedValueException;
+
+abstract class AbstractUnexpectedValueException extends UnexpectedValueException implements UnexpectedValueExceptionInterface
+{
+
+}
